b7bd83d38437ac9ae9fb97f0d3276701adb60fb8
vfydr2m9wk-ops <vfydr2m9wk@privaterelay.appleid.com>
2026-09-07 17:31:29 -0300

    Wire Plain Text T1 essentials into app

--- apps/txt/app.js
+++ apps/txt/app.js
@@ -1,7 +1,7 @@
 (function(g){'use strict';
-const NS=g.InkDOS2,controls=NS.TxtControls.create(),E=controls.elements;
-const frameMenu=NS.AppFrame.bindDrawer({trigger:E.menuBtn,drawer:E.menu,backdrop:E.backdrop,closeButton:E.closeMenu,beforeOpen:controls.closeListMenu});NS.AppFrame.bindHorizontalScroller(E.toolbar);
+const NS=g.InkDOS2,controls=NS.TxtControls.create(),E=controls.elements;let t1=null;
+const frameMenu=NS.AppFrame.bindDrawer({trigger:E.menuBtn,drawer:E.menu,backdrop:E.backdrop,closeButton:E.closeMenu,beforeOpen:()=>{controls.closeListMenu();t1?.closeTools()}});NS.AppFrame.bindHorizontalScroller(E.toolbar);
 const state=NS.TxtState.create(),editor=NS.TxtEditorController.create({elements:E,state}),files=NS.TxtFileController.create({elements:E,state,editor});
 editor.setCheckpointHandler(files.checkpoint);new NS.ContentViewportAdapter(document.querySelector('.content-viewport'),editor.setMetrics);
-controls.bind({editor,files,state},frameMenu);NS.TxtAppDebug=Object.freeze({state,...editor.debug(),...files.debug()});files.initialize();
+controls.bind({editor,files,state},frameMenu);t1=NS.TxtT1Essentials.create({elements:E,state,editor,files,controls});t1.install();NS.TxtAppDebug=Object.freeze({state,...editor.debug(),...files.debug(),txtT1:t1.debug()});files.initialize();
 })(globalThis);
