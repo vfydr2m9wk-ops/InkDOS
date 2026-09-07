acb9dc2aa00e029cfe5bb4646f5645e4598400a3
vfydr2m9wk-ops <vfydr2m9wk@privaterelay.appleid.com>
2026-09-07 16:08:23 -0300

    Rotate offline cache for 2.0.11

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 'use strict';
-const CACHE_NAME='inkdos-v2.0.10-pdf-frame-alignment-seq78';
+const CACHE_NAME='inkdos-v2.0.11-single-save-delivery-seq79';
 const APP_SHELL=[
   "./apps/pdf/app.js",
   "./apps/pdf/assets/pdf.svg",
