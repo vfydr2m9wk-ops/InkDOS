a5db185c265f2690dee0985bba1b87c6d2bcf1d0
vfydr2m9wk-ops <vfydr2m9wk@privaterelay.appleid.com>
2026-09-07 21:09:12 -0300

    Preserve canonical 2.0.12 cache name with DOC-D2 assets

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 'use strict';
-const CACHE_NAME='inkdos-v2.0.12-doc-d2-rtf-seq81';
+const CACHE_NAME='inkdos-v2.0.12-modularity-epub-polish-seq80';
 const APP_SHELL=[
   "./apps/pdf/app.js",
   "./apps/pdf/assets/pdf.svg",
