8edaffcc77e84e21c2e8cf05c1dc63ba2336113f
vfydr2m9wk-ops <vfydr2m9wk@privaterelay.appleid.com>
2026-09-24 11:55:18 -0300

    2.6.0: route Plain Text open through FSA and replacement guard

--- apps/txt/app.js
+++ apps/txt/app.js
@@ -1,7 +1,7 @@
 (function(g){'use strict';
 const NS=g.InkDOS2;let t1=null,t2=null;const controls=NS.TxtControls.create(),E=controls.elements;
 const frameMenu=NS.AppFrame.bindDrawer({trigger:E.menuBtn,drawer:E.menu,backdrop:E.backdrop,closeButton:E.closeMenu,beforeOpen:()=>{controls.closeListMenu();t1?.closeTools();t2?.close()}});NS.AppFrame.bindHorizontalScroller(E.toolbar);NS.AppFrame.installToolbarRail(E.toolbar);
-const state=NS.TxtState.create(),editor=NS.TxtEditorController.create({elements:E,state}),files=NS.TxtFileController.create({elements:E,state,editor}),commands=NS.TxtCommands.create({state,editor,files,requestOpen:()=>E.fileInput.click()});
+const state=NS.TxtState.create(),editor=NS.TxtEditorController.create({elements:E,state}),files=NS.TxtFileController.create({elements:E,state,editor}),commands=NS.TxtCommands.create({state,editor,files,requestOpen:()=>{if(g.InkDOSFileLaunch?.requestPicker?.(E.fileInput))return true;E.fileInput.click();return true}});g.InkDOSFileLaunch?.setOpenHandler?.(file=>files.openFile(file));
 editor.setCheckpointHandler(files.checkpoint);new NS.ContentViewportAdapter(document.querySelector('.content-viewport'),editor.setMetrics);
 t1=NS.TxtT1Essentials.create({elements:E,state,editor,commands,beforeOpen:()=>{controls.closeListMenu();t2?.close()}});t1.install();t2=NS.TxtT2XmlTools.create({elements:E,state,editor,commands,beforeOpen:()=>t1?.closeTools()});t2.install();controls.bind({editor,files,state,commands},frameMenu,{beforeListOpen:()=>{t1?.closeTools();t2?.close()}});NS.TxtAppDebug=Object.freeze({state,commands,...editor.debug(),...files.debug(),txtT1:t1.debug(),txtT2:t2.debug()});files.initialize();
 })(globalThis);
